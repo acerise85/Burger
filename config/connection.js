28af093e0d72d94a71ef11db8971b56644795b12
acerise <42525915+acerise85@users.noreply.github.com>
2019-02-25 21:55:07 -0700

    Added Heroku JAWS

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,20 @@ let connection = mysql.createConnection({
     database: "burgers_db"
 });
 
-connection.connect(function(err){
-    if (err){
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    })
+}
+
+connection.connect(function (err) {
+    if (err) {
         throw err;
     }
     console.log("connected as id " + connection.threadId);
